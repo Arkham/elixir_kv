652d31d738bb7deca47b8eb5d95faa9f6cdc07d5
Ju Liu <ju.liu@welaika.com>
2015-10-23 00:15:22 +0200

    Add delete action to buckets

--- test/elixir_kv/bucket_test.exs
+++ test/elixir_kv/bucket_test.exs
@@ -1,11 +1,22 @@
 defmodule KV.BucketTest do
   use ExUnit.Case, async: true
 
-  test "stores values by key" do
+  setup do
     {:ok, bucket} = KV.Bucket.start_link
+    {:ok, bucket: bucket}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
     assert KV.Bucket.get(bucket, "milk") == nil
 
     KV.Bucket.put(bucket, "milk", 7)
     assert KV.Bucket.get(bucket, "milk") == 7
+  end
+
+  test "deletes values", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "milk", 7)
+
+    assert KV.Bucket.delete(bucket, "milk") == 7
+    assert KV.Bucket.get(bucket, "milk") == nil
   end
 end
